db885b4489377157825c7a6cd237d1a4f10b9483
Will Kahn-Greene <willkg@mozilla.com>
2014-10-01 13:21:11 -0400

    JS cleanup, remove jquery usage, add inprogress text

--- trends.js
+++ trends.js
@@ -1,17 +1,17 @@
-(function(window, $, _, d3) {
+(function(window, _, d3) {
     'use strict';
 
-    var category_array = [
+    var inputUrl = "https://input.mozilla.org/api/v1/feedback/?date_delta=28d&products=Loop&max=10000";
+    var categoryArray = [
         { name: "other", fullname: "Other" },
-        { name: "disconnected", fullname: "Disconnected" }, 
-        { name: "confusing", fullname: "Confusing" }, 
-        { name: "video_quality", fullname: "Video Quality" }, 
-        { name: "audio_quality", fullname: "Audio Quality" }, 
-        { name: "happy", fullname: "Happy" } 
+        { name: "disconnected", fullname: "Disconnected" },
+        { name: "confusing", fullname: "Confusing" },
+        { name: "video_quality", fullname: "Video Quality" },
+        { name: "audio_quality", fullname: "Audio Quality" },
+        { name: "happy", fullname: "Happy" }
     ];
-    
-    var highlight_box = null;
-    var percent_status = true;
+
+    var highlightBox = null;
 
     function darkenSeries(d) {
         d3.selectAll(".legend-item")
@@ -28,6 +28,7 @@
                         .duration(200);
                 }
             });
+
         d3.selectAll(".bar rect")
             .each(function (c, i) {
                 if (c.name == d.name) {
@@ -41,9 +42,9 @@
                         .style("fill-opacity", 0.6)
                         .duration(200);
                 }
-            });           
+            });
     }
-    
+
     function lightenSeries(d) {
         d3.selectAll(".bar rect")
             .each(function (c, i) {
@@ -52,34 +53,36 @@
                     .style("fill-opacity", 0.6)
                     .duration(200);
             });
+
         d3.selectAll(".legend-item").select("circle")
             .transition()
             .style("fill-opacity", 0.6)
-            .duration(200)            
+            .duration(200);
     }
-    
-    function showFeedback(d) {
-        if (highlight_box !== null) {
-            d3.select(highlight_box)
+
+    function showFeedback(selected, d) {
+        if (highlightBox !== null) {
+            d3.select(highlightBox)
                 .style("stroke-width", "1px")
                 .style("stroke", "#333");
-            $("#inputlist").empty();
-            $("#listheader").empty();
+            d3.select('#inputlist').html('');
+            d3.select('#listheader').html('');
         }
-        if (highlight_box == this) {
-            highlight_box = null;
-            d3.select(this)
+
+        if (highlightBox === selected) {
+            highlightBox = null;
+            d3.select(selected)
                 .style("stroke-width", "1px")
                 .style("stroke", "#333");
-            $("#inputlist").empty();
-            $("#listheader").empty();
+            d3.select('#inputlist').html('');
+            d3.select('#listheader').html('');
         } else {
-            highlight_box = this;
-            d3.select(this)
+            highlightBox = selected;
+            d3.select(selected)
                 .style("stroke-width", "2px")
                 .style("stroke", "#F00");
             var title = d.fullname + " category feedback for week of " + d.week;
-            $("#listheader").text(title);
+            d3.select('#listheader').text(title);
             _.forEach(d.verbatims, function (n, t) {
                 var text = (n > 1)?t + " (x" + n + ")":t;
                 d3.select("#inputlist")
@@ -87,38 +90,50 @@
             });
         }
     }
-    
-    $("#spacer").height($(".chart-header").height() + 40);
-    
-    d3.json("https://input.mozilla.org/api/v1/feedback/?date_delta=70d&products=Loop&max=10000",function (error, json) {
-        //d3.json("hellofeedback.json",function (error, json) {
+
+    function getWidth(selection) {
+        return parseInt(selection.style('width'), 10);
+    }
+
+    function getHeight(selection) {
+        return parseInt(selection.style('height'), 10);
+    }
+
+    function getTop(selection) {
+        return selection.node().getBoundingClientRect().top;
+    }
+
+    d3.select('#spacer').style('height', (getHeight(d3.select('.chart-header')) + 40) + 'px');
+
+    d3.json(inputUrl, function (error, json) {
         var timeformat = d3.time.format("%Y-%0m-%0dT%H:%M:%S");
         var displayformat = d3.time.format("%-m/%-d");
         var max_day = d3.time.format("%w")(timeformat.parse(json.results[0].created));
-        console.log(max_day); // TODO: make weeks dynamic
+
         var responses = _(json.results)
             .map(function (i) {
                 var datetime = timeformat.parse(i.created);
-                var category = i.happy ? "happy" : (i.category == "" ? "other" : i.category); // TODO: not hardcode categories
-                return { 
-                    "datetime": datetime, 
-                    "category": category, 
+                // FIXME: not hardcode categories
+                var category = i.happy ? "happy" : (i.category == "" ? "other" : i.category);
+                return {
+                    "datetime": datetime,
+                    "category": category,
                     "text": i.description
                 };
             })
             .groupBy(function (item) { return timeformat(d3.time.week.floor(item.datetime)); })
-            .map(function (value, key) { 
+            .map(function (value, key) {
                 var y = 0;
                 var categories = _(value)
                     .groupBy("category")
-                    .map( function (v,k) { 
-                        var i = _.findIndex(category_array, {"name": k});
+                    .map( function (v,k) {
+                        var i = _.findIndex(categoryArray, {"name": k});
                         var verb = _.countBy(v, "text");
-                        return { 
-                            "name": k, 
+                        return {
+                            "name": k,
                             "verbatims": verb,
                             "category_id": i,
-                            "fullname": category_array[i].fullname,
+                            "fullname": categoryArray[i].fullname,
                             "value": v.length,
                             "week": displayformat(timeformat.parse(key))
                         };
@@ -134,10 +149,10 @@
                         i.p1 = i.y1 / y * 100;
                         i.p = i.value / y * 100;
                     });
-                return { 
-                    "week": key, 
+                return {
+                    "week": key,
                     "name": displayformat(timeformat.parse(key)),
-                    "categories": categories.value(), 
+                    "categories": categories.value(),
                     "total": y
                 };
             })
@@ -145,9 +160,9 @@
             .filter( function (i) { return i.total > 50; })
             .value();
         console.log(responses);
-        
+
         // HORRAY CHART
-        
+
         var margin = {
             top: 20,
             right: 100,
@@ -186,36 +201,37 @@
             'rgb(230,201,2)', // audio
             'rgb(102,166,30)', // happy
         ];
-    
+
         var selection = d3.select("#chart");
-        var $selection = $(selection[0]);
-        $selection.empty();
-        width = $selection.width();
+        var chartHeader = d3.select('.chart-header');
+
+        // Remove everything under the chart node.
+        selection.html("");
+
+        width = getWidth(selection);
         height = width / aspectRatio;
-        if (height > $(window).height() - $(".chart-header").height() - $(".chart-header").offset().top - 40) {
-            height = Math.max($(window).height() - $(".chart-header").height() - $(".chart-header").offset().top - 40, 400);
+        if (height > window.innerHeight - getHeight(chartHeader) - getTop(chartHeader) - 40) {
+            height = Math.max(window.innerHeight - getHeight(chartHeader) - getTop(chartHeader) - 40, 400);
             width = height * aspectRatio;
         }
-        xScale.domain(responses.map(function(d) {
-            return d.name;
-        }))
+        xScale.domain(responses.map(function(d) { return d.name; }))
             .rangeRoundBands([margin.left, width - margin.right], 0.1, 0.3);
-        
+
         yScale.domain([y_minValue, y_maxValue])
             .range([height - margin.bottom, margin.top]);
-        
+
         var colorScale = d3.scale.ordinal().range(colorArray)
-            .domain(_.pluck(category_array, "name"));
-        
+            .domain(_.pluck(categoryArray, "name"));
+
         var svg = selection.append('svg');
         var g = svg.append('g');
         g.append("g").attr("class", "x axis");
         g.append("g").attr("class", "y axis");
         g.append("g").attr("class", "bars");
-        
+
         svg.attr("width", width)
             .attr("height", height);
-        
+
         g.select(".x.axis")
             .attr("transform", "translate(0," + (height - margin.bottom) + ")")
             .call(xAxis)
@@ -225,7 +241,7 @@
             .classed("axis-label", true)
             .attr("y", 5)
             .attr("transform", "translate(" + ((margin.left + width - margin.right) / 2) + "," + 30 + ")");
-        
+
         g.select(".y.axis")
             .attr("transform", "translate(" + margin.left + ",0)")
             .call(yAxis)
@@ -236,7 +252,7 @@
             .attr("dy", ".71em")
             .style("text-anchor", "end")
             .text(yLabel);
-        
+
         var bar = g.select(".bars")
             .selectAll(".bar")
             .data(responses)
@@ -245,6 +261,7 @@
             .attr("transform", function(d) {
                 return "translate(" + xScale(d.name) + ",0)";
             });
+
         bar.selectAll(".box")
             .data(function (d) { return d.categories; })
             .enter()
@@ -262,30 +279,31 @@
             .style("fill-opacity", 0.6)
             .on("mouseenter", darkenSeries)
             .on("mouseleave", lightenSeries)
-            .on("click", showFeedback)
+            .on("click", function (d) { return showFeedback(this, d); })
             .append("svg:title")
             .text(function(d) {
-                return "Category: " + d.fullname + " " + d.value 
+                return "Category: " + d.fullname + " " + d.value
                     + " users (" + Math.round(d.p) + "%)";
             });
-        
+
         g.selectAll(".axis path")
             .style("fill", "none")
             .style("stroke", "#000");
-        
+
         g.selectAll(".y.axis line")
             .style("fill", "none")
             .style("stroke", "#000");
-        
+
         var legend = svg.append("g")
             .attr("class", "legend")
             .attr("transform", "translate(" + (width - margin.right + 5) + ", " + (margin.top + 10) + ")");
-        
+
         var legend_item = legend.selectAll(".legend-item")
-            .data(category_array.reverse())
+            .data(categoryArray.reverse())
             .enter()
             .append("g")
             .attr("class", "legend-item");
+
         legend_item.append("text")
             .text(function(d) {
                 return d.fullname;
@@ -294,7 +312,7 @@
                 return i * 2 + "em";
             })
             .attr("dx", "1em");
-        
+
         legend_item.append("circle")
             .attr("cy", function(d, i) {
                 return (i * 2 - 0.25) + "em";
@@ -307,33 +325,8 @@
             .style("fill-opacity", 0.6)
             .style("stroke", "#999")
             .style("stroke-width", "1px");
-        
+
         legend_item.on("mouseenter", darkenSeries).on("mouseleave", lightenSeries);
     });
-    
-    // function togglePercent () {
-    //     if (percent_status) {
-    //         d3.selectAll(".bar rect")
-    //             .transition()
-    //             .attr("y", function(d) {
-    //                 return yScale(d.y1);
-    //             })
-    //             .attr("height", function(d) {
-    //                 return yScale(d.y0) - yScale(d.y1);
-    //             })
-    //             .duration(500);
-    //         percent_status = false;
-    //     } else {
-    //         d3.selectAll(".bar rect")
-    //             .transition()
-    //             .attr("y", function(d) {
-    //                 return yScale(d.p1);
-    //             })
-    //             .attr("height", function(d) {
-    //                 return yScale(d.p0) - yScale(d.p1);
-    //             })
-    //             .duration(500);
-    //         percent_status = true;
-    //     }
-    // }
-}(window, jQuery, _, d3));
+
+}(window, _, d3));
