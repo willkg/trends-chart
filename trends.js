153e9d5415e58ce77d1ac4e1e8ccbe211af2681c
Will Kahn-Greene <willkg@mozilla.com>
2014-10-03 14:49:19 -0400

    Rework to look at trending words for Firefox for Android

--- trends.js
+++ trends.js
@@ -1,15 +1,359 @@
 (function(window, _, d3) {
     'use strict';
 
-    var inputUrl = "https://input.mozilla.org/api/v1/feedback/?date_delta=28d&products=Loop&max=10000";
-    var categoryArray = [
-        { name: "other", fullname: "Other" },
-        { name: "disconnected", fullname: "Disconnected" },
-        { name: "confusing", fullname: "Confusing" },
-        { name: "video_quality", fullname: "Video Quality" },
-        { name: "audio_quality", fullname: "Audio Quality" },
-        { name: "happy", fullname: "Happy" }
+    var inputUrl = "https://input.mozilla.org/api/v1/feedback/?happy=0&products=Firefox for Android&locales=en-US&max=10000";
+
+    var stopWords = [
+        'able',
+        'about',
+        'above',
+        'across',
+        'after',
+        'afterwards',
+        'again',
+        'against',
+        'all',
+        'almost',
+        'alone',
+        'along',
+        'already',
+        'also',
+        'although',
+        'always',
+        'am',
+        'among',
+        'amongst',
+        'amoungst',
+        'amount',
+        'an',
+        'and',
+        'another',
+        'any',
+        'anyhow',
+        'anyone',
+        'anything',
+        'anyway',
+        'anywhere',
+        'are',
+        'around',
+        'as',
+        'at',
+        'back',
+        'be',
+        'became',
+        'because',
+        'become',
+        'becomes',
+        'becoming',
+        'been',
+        'before',
+        'beforehand',
+        'behind',
+        'being',
+        'below',
+        'beside',
+        'besides',
+        'between',
+        'beyond',
+        'bill',
+        'both',
+        'bottom',
+        'browser',
+        'but',
+        'by',
+        'call',
+        'can',
+        'can\'t',
+        'cannot',
+        'co',
+        'computer',
+        'con',
+        'could',
+        'couldnt',
+        'cry',
+        'de',
+        'describe',
+        'detail',
+        'do',
+        'does',
+        'doesn\'t',
+        'don\'t',
+        'done',
+        'dont',
+        'down',
+        'due',
+        'during',
+        'each',
+        'eg',
+        'eight',
+        'either',
+        'eleven',
+        'else',
+        'elsewhere',
+        'empty',
+        'enough',
+        'etc',
+        'even',
+        'ever',
+        'every',
+        'everyone',
+        'everything',
+        'everywhere',
+        'except',
+        'few',
+        'fifteen',
+        'fify',
+        'fill',
+        'find',
+        'fire',
+        'firefox',
+        'first',
+        'five',
+        'fix',
+        'for',
+        'former',
+        'formerly',
+        'forty',
+        'found',
+        'four',
+        'from',
+        'front',
+        'full',
+        'further',
+        'get',
+        'give',
+        'go',
+        'had',
+        'has',
+        'hasnt',
+        'have',
+        'he',
+        'hello',
+        'hence',
+        'her',
+        'here',
+        'hereafter',
+        'hereby',
+        'herein',
+        'hereupon',
+        'hers',
+        'herse',
+        'hi',
+        'him',
+        'himse',
+        'his',
+        'how',
+        'however',
+        'hundred',
+        'i\'m',
+        'ie',
+        'if',
+        'in',
+        'inc',
+        'indeed',
+        'interest',
+        'into',
+        'is',
+        'it',
+        'it\'s',
+        'its',
+        'itse',
+        'just',
+        'keep',
+        'keeps',
+        'last',
+        'latter',
+        'latterly',
+        'least',
+        'less',
+        'like',
+        'ltd',
+        'made',
+        'make',
+        'many',
+        'may',
+        'me',
+        'meanwhile',
+        'might',
+        'mill',
+        'mine',
+        'more',
+        'moreover',
+        'most',
+        'mostly',
+        'move',
+        'much',
+        'must',
+        'my',
+        'myse',
+        'name',
+        'namely',
+        'neither',
+        'never',
+        'nevertheless',
+        'next',
+        'nine',
+        'no',
+        'nobody',
+        'none',
+        'noone',
+        'nor',
+        'not',
+        'nothing',
+        'now',
+        'nowhere',
+        'of',
+        'off',
+        'often',
+        'on',
+        'once',
+        'one',
+        'only',
+        'onto',
+        'or',
+        'other',
+        'others',
+        'otherwise',
+        'our',
+        'ours',
+        'ourselves',
+        'out',
+        'over',
+        'own',
+        'page',
+        'part',
+        'per',
+        'perhaps',
+        'please',
+        'put',
+        'rather',
+        're',
+        'same',
+        'see',
+        'seem',
+        'seemed',
+        'seeming',
+        'seems',
+        'serious',
+        'several',
+        'she',
+        'should',
+        'show',
+        'side',
+        'since',
+        'sincere',
+        'site',
+        'six',
+        'sixty',
+        'so',
+        'some',
+        'somehow',
+        'someone',
+        'something',
+        'sometime',
+        'sometimes',
+        'somewhere',
+        'still',
+        'such',
+        'system',
+        'take',
+        'ten',
+        'than',
+        'that',
+        'the',
+        'their',
+        'them',
+        'themselves',
+        'then',
+        'thence',
+        'there',
+        'thereafter',
+        'thereby',
+        'therefore',
+        'therein',
+        'thereupon',
+        'these',
+        'they',
+        'thick',
+        'thin',
+        'third',
+        'this',
+        'those',
+        'though',
+        'three',
+        'through',
+        'throughout',
+        'thru',
+        'thus',
+        'time',
+        'to',
+        'together',
+        'too',
+        'top',
+        'toward',
+        'towards',
+        'twelve',
+        'twenty',
+        'two',
+        'un',
+        'under',
+        'until',
+        'up',
+        'upon',
+        'us',
+        'use',
+        'using',
+        'very',
+        'via',
+        'want',
+        'was',
+        'we',
+        'web',
+        'well',
+        'were',
+        'what',
+        'whatever',
+        'when',
+        'whence',
+        'whenever',
+        'where',
+        'whereafter',
+        'whereas',
+        'whereby',
+        'wherein',
+        'whereupon',
+        'wherever',
+        'whether',
+        'which',
+        'while',
+        'whither',
+        'who',
+        'whoever',
+        'whole',
+        'whom',
+        'whose',
+        'why',
+        'will',
+        'with',
+        'within',
+        'without',
+        'work',
+        'would',
+        'yet',
+        'you',
+        'you\'re',
+        'your',
+        'yours',
+        'yourself',
+        'yourselves'
     ];
+
+    var synonyms = {
+        'crashes': 'crash',
+        'crashing': 'crash',
+        'tabs': 'tab',
+        'videos': 'video'
+    };
 
     var highlightBox = null;
 
@@ -24,7 +368,7 @@
                 } else {
                     d3.select(this).select("circle")
                         .transition()
-                        .style("fill-opacity", 0.6)
+                        .style("fill-opacity", 0.4)
                         .duration(200);
                 }
             });
@@ -39,7 +383,7 @@
                 } else {
                     d3.select(this)
                         .transition()
-                        .style("fill-opacity", 0.6)
+                        .style("fill-opacity", 0.4)
                         .duration(200);
                 }
             });
@@ -50,13 +394,13 @@
             .each(function (c, i) {
                 d3.select(this)
                     .transition()
-                    .style("fill-opacity", 0.6)
+                    .style("fill-opacity", 0.4)
                     .duration(200);
             });
 
         d3.selectAll(".legend-item").select("circle")
             .transition()
-            .style("fill-opacity", 0.6)
+            .style("fill-opacity", 0.4)
             .duration(200);
     }
 
@@ -103,6 +447,37 @@
         return selection.node().getBoundingClientRect().top;
     }
 
+    function tokenize(text) {
+        var tokens;
+
+        // split the text into tokens
+        tokens = text.split(/[\s\?\!,~\(\)\-\\;\*\$\^\|\"<>{}]+/);
+
+        tokens = _(tokens)
+            .map(function (token) {
+                // Lowercase, trim and apply synonyms
+                token = token.toLocaleLowerCase().trim();
+                if (synonyms.hasOwnProperty(token)) {
+                    token = synonyms[token];
+                }
+                return token;
+            })
+            .filter(function (token) {
+                // Limit the length, require at least one letter and
+                // make sure it's not a stop word.
+                return (token.length > 1
+                        && token.length < 15
+                        && token.search(/[a-z]/) != -1
+                        && !_.contains(stopWords, token)
+                       );
+            })
+            .compact()
+            .uniq()
+            .value();
+
+        return tokens;
+    }
+
     d3.select('#spacer').style('height', (getHeight(d3.select('.chart-header')) + 40) + 'px');
 
     d3.json(inputUrl, function (error, json) {
@@ -110,56 +485,107 @@
         var displayformat = d3.time.format("%-m/%-d");
         var max_day = d3.time.format("%w")(timeformat.parse(json.results[0].created));
 
-        var responses = _(json.results)
-            .map(function (i) {
-                var datetime = timeformat.parse(i.created);
-                // FIXME: not hardcode categories
-                var category = i.happy ? "happy" : (i.category == "" ? "other" : i.category);
+        var responses = json.results;
+
+        // Go through and tokenize the description for all the
+        // responses.
+        responses.forEach(function(item) {
+            item.description_tokens = tokenize(item.description);
+        });
+
+        responses = _(responses)
+            .map(function (item) {
+                // Simplify the response structure and throw away the
+                // stuff we don't need
                 return {
-                    "datetime": datetime,
-                    "category": category,
-                    "text": i.description
+                    "datetime": timeformat.parse(item.created),
+                    "tokens": item.description_tokens,
+                    "text": item.description
                 };
             })
-            .groupBy(function (item) { return timeformat(d3.time.week.floor(item.datetime)); })
-            .map(function (value, key) {
+            .groupBy(function (item) {
+                // Break responses into groups by week
+                return timeformat(d3.time.week.floor(item.datetime));
+            })
+            .map(function (responses, week) {
+                // Figure out the important words for each week, figure
+                // out counts and associated responses and y values
                 var y = 0;
-                var categories = _(value)
-                    .groupBy("category")
-                    .map( function (v,k) {
-                        var i = _.findIndex(categoryArray, {"name": k});
-                        var verb = _.countBy(v, "text");
+                var wordsThisWeek = _(responses)
+                    .map(function(item) {
+                        return item.tokens;
+                    })
+                    .flatten()
+                    .countBy()
+                    .value();
+
+                // Note: When using lodash map here, it would somehow
+                // iterate over undefines. I couldn't figure out why,
+                // so I just wrote the equivalent forEach and it works
+                // fine now.
+                var newWords = [];
+                Object.keys(wordsThisWeek).forEach(function (word) {
+                    newWords.push({
+                        'name': word,
+                        'verbatims': [],
+                        'fullname': word,
+                        'value': wordsThisWeek[word]
+                    });
+                });
+                wordsThisWeek = newWords;
+
+                wordsThisWeek = _(wordsThisWeek)
+                    .sortBy('value')
+                    .last(10)
+                    .sortBy('name')
+                    .forEach(function (item) {
+                        item.y0 = y;
+                        y += item.value;
+                        item.y1 = y;
+
+                        item.p0 = item.y0;
+                        item.p1 = item.y1;
+                        item.p = item.value;
+                    })
+                    .value();
+
+                return {
+                    'week': week,
+                    'name': displayformat(timeformat.parse(week)),
+                    'words': wordsThisWeek,
+                    'responses': responses,
+                    'total': y
+                };
+            })
+            .sortBy('week')
+            .value();
+
+        // When we pull data from Input, we pull 21 days. However,
+        // that's days and not weeks, so there's a granularity mismatch
+        // between how we pull data and how we display it. In order to deal
+        // with that, we check to see if today is sunday and if not, we
+        // drop the first week in the array because it's only partial.
+        if ((new Date()).getDay() !== 0) {
+            responses.shift();
+        }
+
+        var categoryArray = _(responses)
+            .map(function (week) {
+                return _(week.words)
+                    .map(function (word) {
                         return {
-                            "name": k,
-                            "verbatims": verb,
-                            "category_id": i,
-                            "fullname": categoryArray[i].fullname,
-                            "value": v.length,
-                            "week": displayformat(timeformat.parse(key))
+                            'name': word.name,
+                            'fullname': word.fullname
                         };
                     })
-                    .sortBy("category_id")
-                    .forEach( function (i) {
-                        i.y0 = y;
-                        y += i.value;
-                        i.y1 = y;
-                    })
-                    .forEach( function (i) {
-                        i.p0 = i.y0 / y * 100;
-                        i.p1 = i.y1 / y * 100;
-                        i.p = i.value / y * 100;
-                    });
-                return {
-                    "week": key,
-                    "name": displayformat(timeformat.parse(key)),
-                    "categories": categories.value(),
-                    "total": y
-                };
+                    .value();
             })
-            .sortBy("week")
-            .filter( function (i) { return i.total > 50; })
+            .flatten()
+            .uniq(function (item) {
+                return item.name;
+            })
+            .sortBy('name')
             .value();
-        console.log(responses);
 
         // HORRAY CHART
 
@@ -174,7 +600,7 @@
         aspectRatio = 1.3, // not even setable, I'm lazy yo #TODO
         fillColor = "#c13832",
         y_minValue = 0,
-        y_maxValue = 100, // default height
+        y_maxValue = d3.max(responses, function (item) { return item.total; }),
         xScale = d3.scale.ordinal(),
         yScale = d3.scale.linear(),
         xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(6, 0),
@@ -185,22 +611,14 @@
         yValue = function(d) {
             return d;
         },
-        yLabel = "Percentage",
+        yLabel = "Response count",
         xLabel = "Week of",
         xTooltip = function(d) {
             return null;
         },
         barTooltip = function(d) {
             return null;
-        },
-        colorArray = [
-            'rgb(120,120,120)', // other
-            'rgb(231,60,65)', // disconnected
-            'rgb(117,112,179)', // confusing
-            'rgb(217,95,2)', // video
-            'rgb(230,201,2)', // audio
-            'rgb(102,166,30)', // happy
-        ];
+        };
 
         var selection = d3.select("#chart");
         var chartHeader = d3.select('.chart-header');
@@ -214,14 +632,17 @@
             height = Math.max(window.innerHeight - getHeight(chartHeader) - getTop(chartHeader) - 40, 400);
             width = height * aspectRatio;
         }
+
         xScale.domain(responses.map(function(d) { return d.name; }))
             .rangeRoundBands([margin.left, width - margin.right], 0.1, 0.3);
 
         yScale.domain([y_minValue, y_maxValue])
             .range([height - margin.bottom, margin.top]);
 
-        var colorScale = d3.scale.ordinal().range(colorArray)
-            .domain(_.pluck(categoryArray, "name"));
+        // var colorScale = d3.scale.ordinal().range(colorArray)
+        //     .domain(_.pluck(categoryArray, "name"));
+        var colorScale = d3.scale.category20c()
+            .domain(_.pluck(categoryArray, 'name'));
 
         var svg = selection.append('svg');
         var g = svg.append('g');
@@ -263,7 +684,7 @@
             });
 
         bar.selectAll(".box")
-            .data(function (d) { return d.categories; })
+            .data(function (d) { return d.words; })
             .enter()
             .append("rect")
             .attr("width", xScale.rangeBand())
@@ -282,8 +703,7 @@
             .on("click", function (d) { return showFeedback(this, d); })
             .append("svg:title")
             .text(function(d) {
-                return "Category: " + d.fullname + " " + d.value
-                    + " users (" + Math.round(d.p) + "%)";
+                return "Word: '" + d.fullname + "' Number of responses: " + d.value;
             });
 
         g.selectAll(".axis path")
